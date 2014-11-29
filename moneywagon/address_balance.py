558375b1a56b9e4759fb44e797f299395fd362fa
Chris Priest <nbvfour@gmail.com>
2014-11-29 09:50:49 -0800

    more fetchers

--- moneywagon/address_balance.py
+++ moneywagon/address_balance.py
@@ -3,6 +3,8 @@ from .fetcher import Fetcher
 class BlockrAddressBalance(Fetcher):
     def get_balance(self, crypto, address):
         crypto = crypto.lower()
+        if crypto not in ['btc', 'ltc', 'ppc', 'mec', 'qrk', 'dgc', 'tbtc']:
+            raise SkipThisFetcher("Blockr.io does not support this crypto: " + crypto)
         url = "http://%s.blockr.io/api/v1/address/info/%s" % (crypto, address)
         response = self.fetch_url(url)
         return response.json()['data']['balance'], 'blockr.io'
@@ -15,3 +17,9 @@ class BlockChainInfoAddressBalance(Fetcher):
         url = "http://blockchain.info/address/%s?format=json" % address
         response = self.fetch_url(url)
         return float(response.json()['final_balance']) * 1e-8, 'blockchain.info'
+
+class DogeChainInfoAddressBalance(Fetcher):
+    def get_balance(self, crypto, address):
+        url = "https://dogechain.info/chain/Dogecoin/q/addressbalance/" + address
+        response = self.fetch_url(url)
+        return float(response.content), 'dogechain.info'
--- moneywagon/transaction_push.py
+++ moneywagon/transaction_push.py
@@ -1,5 +1,10 @@
-from .fetcher import Fetcher
+from .fetcher import Fetcher, SkipThisFetcher
 
 class BlockrPushTransaction(Fetcher):
     def push_transaction(self, crypto, tx):
+        crypto = crypto.lower()
+        if crypto not in ['btc', 'ltc', 'mec', 'qrk', 'dgc', 'tbtc']:
+            raise SkipThisFetcher("Blockr.io does not support this crypto: " + crypto)
         url = "http://%s.blockr.io/api/v1/tx/push" % crypto
+        self.fetch_url(url)
+        raise NotFinished()
