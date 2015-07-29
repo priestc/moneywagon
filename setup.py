cd0afa89e3c19658241af45ff8b84b819ecb5c4c
Chris Priest <cp368202@ohiou.edu>
2015-07-29 13:02:01 -0700

    another version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="moneywagon",
-    version='1.2.1',
+    version='1.2.2',
     description='Toolset for getting the exchange rate for cryptocurrencies',
     long_description=open('README.md').read(),
     author='Chris Priest',
