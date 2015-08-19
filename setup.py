0fc6e504c7798bcc5fe3820ba47c7a95044eb99c
Chris Priest <cp368202@ohiou.edu>
2015-08-19 15:25:04 -0700

    fixed requirements for py2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 import sys
 
 if sys.version_info <= (3,1):
-    extra_install = ['futures',]
+    extra_install = ['futures']
 else:
     extra_install = []
 
@@ -31,6 +31,6 @@ setup(
         'requests',
         'pytz',
         'arrow',
-        'pybitcointools'
+        'bitcoin'
     ] + extra_install
 )
