da6756046f2e453d1665483d57b581d6d4747238
Chris Priest <cp368202@ohiou.edu>
2015-09-03 00:14:24 -0700

    added working bip38 support (python3 only)

--- moneywagon/bip38.py
+++ moneywagon/bip38.py
@@ -1,11 +1,87 @@
 from Crypto.Cipher import AES
+from hashlib import sha256
 from binascii import unhexlify, hexlify
 from bitcoin import (
-    privtopub, pubtoaddr, encode_privkey, get_privkey_format, sha256,
-    hex_to_b58check, b58check_to_hex
+    privtopub, pubtoaddr, encode_privkey, get_privkey_format,
+    hex_to_b58check, b58check_to_hex, encode_pubkey
 )
 
 try:
     import scrypt
 except ImportError:
     raise ImportError("Scrypt is required for BIP38 support: pip install scrypt")
+
+long = int
+
+def encrypt(privkey,passphrase):
+    """
+    BIP0038 non-ec-multiply encryption. Returns BIP0038 encrypted privkey.
+    """
+    privformat = get_privkey_format(privkey)
+    if privformat in ['wif_compressed','hex_compressed']:
+        compressed = True
+        flagbyte = b'\xe0'
+        if privformat == 'wif_compressed':
+            privkey = encode_privkey(privkey,'hex_compressed')
+            privformat = get_privkey_format(privkey)
+    if privformat in ['wif', 'hex']:
+        compressed = False
+        flagbyte = b'\xc0'
+    if privformat == 'wif':
+        privkey = encode_privkey(privkey,'hex')
+        privformat = get_privkey_format(privkey)
+
+    pubkey = privtopub(privkey)
+    addr = pubtoaddr(pubkey)
+
+    salt          = sha256(sha256(bytes(addr,'ascii')).digest()).digest()[0:4]
+    key           = scrypt.hash(passphrase, salt, 16384, 8, 8)
+    (derivedhalf1, derivedhalf2) = (key[:32], key[32:])
+
+    aes             = AES.new(derivedhalf2)
+    encryptedhalf1 = aes.encrypt(unhexlify('%0.32x' % (long(privkey[0:32], 16) ^ long(hexlify(derivedhalf1[0:16]), 16))))
+    encryptedhalf2 = aes.encrypt(unhexlify('%0.32x' % (long(privkey[32:64], 16) ^ long(hexlify(derivedhalf1[16:32]), 16))))
+
+    payload    = b'\x01' + b'\x42' + flagbyte + salt + encryptedhalf1 + encryptedhalf2
+    checksum   = sha256(sha256(payload).digest()).digest()[:4] # b58check for encrypted privkey
+    privatkey  = hexlify(payload + checksum).decode('ascii')
+    return hex_to_b58check(privatkey)
+
+
+
+
+def decrypt(encrypted_privkey,passphrase):
+    '''BIP0038 non-ec-multiply decryption. Returns hex privkey.'''
+    d = unhexlify(b58check_to_hex(encrypted_privkey))
+    d = d[2:]
+    flagbyte = d[0:1]
+    d = d[1:]
+    # respect flagbyte, return correct pair
+
+    if flagbyte == b'\xc0':
+        compressed = False
+    if flagbyte == b'\xe0':
+        compressed = True
+    addresshash = d[0:4]
+    d = d[4:-4]
+    key = scrypt.hash(passphrase,addresshash, 16384, 8, 8)
+    derivedhalf1 = key[0:32]
+    derivedhalf2 = key[32:64]
+    encryptedhalf1 = d[0:16]
+    encryptedhalf2 = d[16:32]
+    aes = AES.new(derivedhalf2)
+    decryptedhalf2 = aes.decrypt(encryptedhalf2)
+    decryptedhalf1 = aes.decrypt(encryptedhalf1)
+    priv = decryptedhalf1 + decryptedhalf2
+    priv = unhexlify('%064x' % (long(hexlify(priv), 16) ^ long(hexlify(derivedhalf1), 16)))
+    pub = privtopub(priv)
+    if compressed:
+        pub = encode_pubkey(pub,'hex_compressed')
+    addr = pubtoaddr(pub)
+    if sha256(sha256(bytes(addr,'ascii')).digest()).digest()[0:4] != addresshash:
+        raise Exception('Addresshash Error')
+    else:
+        if compressed:
+            return encode_privkey(priv, 'hex_compressed')
+        else:
+            return encode_privkey(priv, 'hex')
