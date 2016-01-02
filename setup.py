fd5b4385cedcb511ebaeb1b7d875a022809e23f6
Chris Priest <cp368202@ohiou.edu>
2016-01-01 21:59:43 -0800

    removed print, version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name="moneywagon",
-    version='1.8.0',
+    version='1.8.1',
     description='Next Generation Cryptocurrency Platform',
     long_description=open('README.md').read(),
     author='Chris Priest',
