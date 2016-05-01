030f1378ae33222a4b3c9dd7927150124044ed0d
Chris Priest <cp368202@ohiou.edu>
2016-04-30 23:14:39 -0700

    added code to handle status code 429

--- moneywagon/core.py
+++ moneywagon/core.py
@@ -80,6 +80,9 @@ class Service(object):
         if response.status_code == 503:
             raise SkipThisService("503 - Temporarily out of service.")
 
+        if response.status_code == 429:
+            raise SkipThisService("429 - Too many requests")
+
         if method == 'get':
             self.responses[url] = response # cache for later
 
