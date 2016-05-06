7ec6e85a93dad632bbd580c0c3b24c371dc0aa13
Chris Priest <cp368202@ohiou.edu>
2016-05-06 15:14:26 -0700

    version bump

--- moneywagon/core.py
+++ moneywagon/core.py
@@ -5,7 +5,7 @@ import time
 
 from concurrent import futures
 
-useragent = 'moneywagon 1.10.6'
+useragent = 'moneywagon 1.10.7'
 
 class ServiceDisagreement(Exception):
     pass
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name="moneywagon",
-    version='1.10.6',
+    version='1.10.7',
     description='Next Generation Cryptocurrency Platform',
     long_description=open('README.md').read(),
     author='Chris Priest',
