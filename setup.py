df8d3e9247fbe7a9c17265141355cc62b8c5d293
Chris Priest <cp368202@ohiou.edu>
2016-05-22 12:59:14 -0700

    version bump 10 1.10.11

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name="moneywagon",
-    version='1.10.10',
+    version='1.10.11',
     description='Next Generation Cryptocurrency Platform',
     long_description=open('README.md').read(),
     author='Chris Priest',
