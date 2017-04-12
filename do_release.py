ea1f704176b121fd9ac32e2f5137144958a0335c
Chris Priest <nbvfour@gmail.com>
2017-04-12 13:20:23 -0700

    Made release 1.14.0

--- do_release.py
+++ do_release.py
@@ -41,3 +41,5 @@ with open("README.md", 'w') as f, open("README_template.md") as t:
 
 call(["python", "setup.py", "sdist", "upload"])
 call(["python", "setup.py", "develop"])
+call(["git", "commit", "-am", "'Made release %s" % version])
+call(["git", "push"])
