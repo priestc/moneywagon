413b2f74eaf5600500c280b1b8284f753bc7f7f2
Chris Priest <cp368202@ohiou.edu>
2017-06-18 22:10:15 -0600

    Made release 1.16.17

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.version_info <= (3,6):
 
 setup(
     name="moneywagon",
-    version='1.16.16',
+    version='1.16.17',
     description='Next Generation Cryptocurrency Platform',
     long_description=open('README.md').read(),
     author='Chris Priest',
